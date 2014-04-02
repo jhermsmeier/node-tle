b8bf43974379ab76030c67b3a8d940d110844d92
jhermsmeier <jhermsmeier@googlemail.com>
2014-04-02 17:30:46 +1300

    Updated test/parseDate: Took TZ out of equation
    Damn time zones, failing tests.

--- test/parseDate.js
+++ test/parseDate.js
@@ -9,26 +9,26 @@ describe( 'TLE.parseDate', function() {
   
   it( 'Hell, I do hate writing tests', function() {
     var date = TLE.parseDate( '14006.78411147' )
-    assert.equal( date.getFullYear(), 2014 )
-    assert.equal( date.getDay(), 2 )
+    assert.equal( date.getUTCFullYear(), 2014 )
+    assert.equal( date.getUTCDay(), 1 )
   })
   
   it( 'should ignore whitespace between epoch and days', function() {
     var date = TLE.parseDate( '14 006.78411147' )
-    assert.equal( date.getFullYear(), 2014 )
-    assert.equal( date.getDay(), 2 )
+    assert.equal( date.getUTCFullYear(), 2014 )
+    assert.equal( date.getUTCDay(), 1 )
   })
   
   it( 'should drop to last century', function() {
     var date = TLE.parseDate( '86 046.78411147' )
-    assert.equal( date.getFullYear(), 1986 )
+    assert.equal( date.getUTCFullYear(), 1986 )
   })
   
   it( 'should go back to the future', function() {
-    var year = new Date().getFullYear()
+    var year = new Date().getUTCFullYear()
     var epoch = year % 100 + 1
     var date = TLE.parseDate( epoch + '046.78411147' )
-    assert.equal( date.getFullYear(), year + 1 )
+    assert.equal( date.getUTCFullYear(), year + 1 )
   })
   
 })
