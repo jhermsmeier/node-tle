84369ee0a0a91a66f9eeb351047f780cd0b538bc
Jonas Hermsmeier <jhermsmeier@gmail.com>
2016-10-26 11:08:40 +0200

    Update test/parse: Add assertion

--- test/parse.js
+++ test/parse.js
@@ -11,6 +11,28 @@ describe( 'TLE.parse', function() {
 
     var tle = TLE.parse( set )
 
+    var expected = {
+      name: 'ISS (ZARYA)',
+      number: 25544,
+      class: 'U',
+      id: '98067A',
+      date: new Date( '2008-09-20T12:25:40.104Z' ),
+      fdmm: -0.00002182,
+      sdmm: 0,
+      drag: -1.1606,
+      ephemeris: 0,
+      esn: 292,
+      inclination: 51.6416,
+      ascension: 247.4627,
+      eccentricity: '0006703',
+      perigee: 130.536,
+      anomaly: 325.0288,
+      motion: 15.721253915,
+      revolution: 6353,
+    }
+
+    assert.deepEqual( tle, expected )
+
   })
 
 })
